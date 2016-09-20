31481d808b97c66688c5e486ab657415b07984a9
andras varadi <andris@varadi.tv>
2016-09-20 18:58:15 +0200

    opening page first-day

--- components/js/script.js
+++ components/js/script.js
@@ -1,5 +1,29 @@
 // JavaScript Document
-function test() {
-    return "hi ";
-	
-}
+$('.owl-carousel').owlCarousel({
+    animateOut: 'fadeOut',
+    loop:true,
+    margin:0,
+    nav:true,
+    items: 1,
+    navText: [
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>"
+   ],
+})
+
+$('.owl-carousel-product').owlCarousel({
+    animateOut: 'fadeOut',
+    loop:true,
+    margin:0,
+    nav:true,
+    items: 1,
+    navText: [
+      "<i class='carousel-left'></i>",
+      "<i class='carousel-right'></i>"
+   ],
+})
+var i = 1;
+$('.owl-carousel-product .owl-dot').each(function(){
+  $(this).text(i);
+  i++;
+});
