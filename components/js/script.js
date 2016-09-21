53c9805bf2ea2c69a938deab4311f0b65be0179d
andras varadi <andris@varadi.tv>
2016-09-21 22:32:50 +0200

    opening navigation update

--- components/js/script.js
+++ components/js/script.js
@@ -29,3 +29,12 @@ $('.owl-carousel-product .owl-dot').each(function(){
 });
 
 $('.box').matchHeight();
+
+
+    var $offCanvas = $('#offcanvas'),
+        $dropdown  = $offCanvas.find('.dropdown');
+    $dropdown.on('show.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').slideDown(350);
+    }).on('hide.bs.dropdown', function(){
+        $(this).find('.dropdown-menu').slideUp(350);
+    });
